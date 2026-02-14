b6c423fe649e0f43e5635a86d5aedcf4b4fe794e
Uday Singh <uday17631@gmail.com>
2026-02-14 11:54:18 -0800

    New changes

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -12,12 +12,3 @@ variable "container_image" {
   type        = string
   description = "Full image name including tag (ACR login server/name:tag)"
 }
-
-variable "static_web_repo_url" {
-  type = string
-}
-
-variable "static_web_branch" {
-  type    = string
-  default = "main"
-}
